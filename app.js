66c5283afa394e16f9c4456a4c3f69904dc20365
Pete Dunlap <petedunlap@gmail.com>
2014-01-10 15:11:56 -0600

    fix yourself git

---
+++ app.js
@@ -0,0 +1,63 @@
+// console.log('HELLO WORLD');
+
+
+
+// var total = 0;
+// for(var i = 2; i<process.argv.length; i++){
+//   // console.log(process.argv[i]);
+//   total+= parseInt(process.argv[i]);
+// }
+// console.log(total);
+
+
+
+  // var fs = require('fs');
+  // var buf = fs.readFileSync(process.argv[2]);
+  // buf = buf.toString()
+  // buf = buf.split('\n').length-1;
+  // console.log(buf);
+
+
+
+
+
+  // var fs = require('fs');
+  // fs.readFile(process.argv[2],function(err, buf){
+  //   var lines = buf.toString().split('\n').length-1;
+  //   console.log(lines);
+  // });
+
+
+
+
+  // var fs = require('fs')
+  // var path = process.argv[2];
+  // var extension = process.argv[3];
+  // fs.readdir(path, function(err, list){
+  //   var re = new RegExp('.*[.]'+extension);
+  //   list.forEach(function(file){
+  //     if(re.test(file)){
+  //       console.log(file);
+  //     }
+  //   });
+  // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
